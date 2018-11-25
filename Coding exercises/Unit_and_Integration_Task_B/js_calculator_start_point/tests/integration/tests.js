328af4dd60ab805f672efe0fb14ce6d42c7e5136
Raphael Ugha <raphaelugha@gmail.com>
2018-11-25 14:37:11 +0000

    correct test module

--- Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -16,7 +16,6 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-
   it('should be able to update the running total', function() {
     running_total = element(by.css('#running_total'));
     element(by.css('#number2')).click();
@@ -39,7 +38,7 @@ describe('calculator functionality', function() {
     element(by.css('#operator_add')).click();
     element(by.css('#number6')).click();
     element(by.css('#operator_multiply')).click();
-    element(by.css('#number0')).click();
+    element(by.css('#number1')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('10');
   })
